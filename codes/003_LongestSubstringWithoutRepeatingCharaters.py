2d113280a79df569cc7e73c6d95296e241bb17d5
Eliasyu <eliasyu0210@gmail.com>
2019-03-10 09:10:45 +0100

    Upload the code of 3rd question, which consists one the question description, the codes, and an issue that I have made

---
+++ codes/003_LongestSubstringWithoutRepeatingCharaters.py
@@ -0,0 +1,29 @@
+#  Given a string, find the length of the longest substring
+#  without repeating characters.
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        else:
+            subString = s[0]
+            subStringLength = len(subString)
+            for i in range(len(s)-1):
+                for j in range(i+1, len(s)):
+                    if s[j] in s[i: j]:
+                        subString = s[i: j]
+                        if subStringLength < len(subString):
+                            subStringLength = len(subString)
+                        break
+                    else:
+                        subString = s[i: j+1]
+                        if subStringLength < len(subString):
+                            subStringLength = len(subString)
+            return subStringLength
+
+# ISSUES:
+# 1. In the first loop the cycles are length of the string minus 1
+#    which means if the string length less than one, the munipulation
+#    is missing. So, make the subString to the first charater of the
+#    string.
