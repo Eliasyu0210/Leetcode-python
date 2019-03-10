04c5de695cc71e2433b03d8e388f10e6433584a1
Eliasyu <eliasyu0210@gmail.com>
2019-03-10 13:00:08 +0100

    Use hash map to solve this problem

--- codes/350_IntersectionOfTwoArrayII.py
+++ codes/350_IntersectionOfTwoArrayII.py
@@ -1,18 +1,20 @@
 # Given two arrays, write a function to compute their intersection.
 
-# Basic implementation:
-# In order to find out the duplicate element in the array,
-# I can't just easily to check existance ofthe pop out value from the 
-# first array in the second array.
+# Basic implementation 1:
+# Using hash map first find out the number of each element in
+# each array, and then the minimum value of element will be 
+# added into the resultant list
+import collections
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num1Count = collections.Counter(nums1)
+        num2Count = collections.Counter(nums2)
         ans = []
-        while len(nums1) != 0:
-            a = nums1.pop()
-            if a in nums2:
-                ans.append(a)
-                nums2.remove(a)
-        ans.sort()
+        for key in num1Count:
+            if key in num2Count:
+                if num1Count[key] >= num2Count[key]:
+                    ans += num2Count[key] * [key]
+                else:
+                    ans += num1Count[key] * [key]
         return ans
-
