df6b55803c8dc96d1232eb0e610426fec2945ba3
Eliasyu <eliasyu0210@gmail.com>
2019-03-11 02:47:13 +0100

    Solve the 1st question using onepass hashmap

---
+++ codes/001_TwoSums_HashMap_OnePass.py
@@ -0,0 +1,22 @@
+# 1. Two Sum
+# Given an array of integers, return indices of the two numbers 
+# such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, 
+# and you may not use the same element twice.
+
+
+# Implementation :
+# We use One pass Hashmap to solve this problem. firstly before each
+# add item into the dictionary, we will check if the complement value
+# already exist in the dict, if yes, return the index of the complement
+# and current index; if no, then add the item into the dictionray  
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dic:
+                index = dic[complement]
+                return [index, i]
+            else:
+                dic[nums[i]] = i
