2e4e33e436d0a12c3d5ca468239e4b7ca9662434
Eliasyu <eliasyu0210@gmail.com>
2019-03-17 15:31:59 +0100

    Add the 711th question and the Hashset solution

---
+++ codes/771_JewelsAndStones_HashSet.py
@@ -0,0 +1,17 @@
+# 771. Jewels and Stones
+# You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+# The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+# Using Hashset, the string of jewels will be seted to a Hashset. which means the time complexity of each search will be O(1)
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        Jset = set(J)
+
+        ans = 0
+        for char in S:
+            if char in Jset:
+                ans += 1
+        return ans
